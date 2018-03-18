55be346a433b3d5052c1e58c3f8180588429f037
Alexandru Stanciu <stanciu.alex74@gmail.com>
2018-03-18 22:24:44 +0000

    OOP1: actually add Car class

--- OOP1-RamblerGarage/Car.cpp
+++ OOP1-RamblerGarage/Car.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include "Car.h"
 
-
+//
 Car::Car()
 {
 }
--- OOP1-RamblerGarage/Car.h
+++ OOP1-RamblerGarage/Car.h
@@ -9,5 +9,7 @@ public:
 	virtual void addPart(Part::Type t, Part::Position p = Part::Position::ANY);
 	Car(std::string m, std::string mod, unsigned year = 1950, unsigned doors = 2);
 	~Car();
+
+	//
 };
 
